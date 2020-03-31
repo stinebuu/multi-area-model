4136cce6d6c20b466fcf510bc79b0d92d7735ac5
Stine Vennemo <stine.vennemo@gmail.com>
2020-03-31 15:41:20 +0200

    update how to run benchmark

--- run_benchmark.py
+++ run_benchmark.py
@@ -5,6 +5,7 @@ import shutil
 import json
 import nest
 
+from run_benchmark_createParams import create_params
 from multiarea_model import MultiAreaModel, MultiAreaModel_3
 from config import base_path, data_path
 
@@ -24,61 +25,9 @@ t_sim = float(sys.argv[3])
 K_scaling = float(sys.argv[4])
 NEST_version = int(sys.argv[5])
 
-d = {}
-conn_params = {#'replace_non_simulated_areas': 'het_poisson_stat',
-               'g': -11.,
-               'K_stable': os.path.join(base_path, 'K_stable.npy'),
-               'fac_nu_ext_TH': 1.2,
-               'fac_nu_ext_5E': 1.125,
-               'fac_nu_ext_6E': 1.41666667,
-               'av_indegree_V1': 3950.}
-input_params = {'rate_ext': 10.}
-neuron_params = {'V0_mean': -150.,
-                 'V0_sd': 50.}
-network_params = {'N_scaling': N_scaling,
-                  'K_scaling': K_scaling,
-                  'fullscale_rates': os.path.join(base_path, 'tests/fullscale_rates.json'),
-                  'input_params': input_params,
-                  'connection_params': conn_params,
-                  'neuron_params': neuron_params}
+print("create parameters")
 
-sim_params = {'t_sim': t_sim,
-              'num_processes': num_processes,
-              'local_num_threads': 1,
-              'recording_dict': {'record_vm': False}}
-
-theory_params = {'dt': 0.1}
-
-if NEST_version == 2:
-    print("NEST version 2.x\n")
-    M = MultiAreaModel(network_params, simulation=True,
-                       sim_spec=sim_params,
-                       theory=True,
-                       theory_spec=theory_params)
-elif NEST_version == 3:
-    print("NEST version 3.0\n")
-    M = MultiAreaModel_3(network_params, simulation=True,
-                         sim_spec=sim_params,
-                         theory=True,
-                         theory_spec=theory_params)
-
-print(M.label)
-print(M.simulation.label)
-
-p, r = M.theory.integrate_siegert()
-
-#M.simulation.simulate()
-
-print("dump parameters\n")
-
-labels_fn = os.path.join(base_path, 'label_files/labels_{}_{}.json'.format(N_scaling, num_processes))
-labels_dict = {'network_label': M.label,
-               'simulation_label': M.simulation.label}
-print(labels_fn)
-
-with open(labels_fn, 'w') as f:
-    json.dump(labels_dict, f)
-
+create_params(N_scaling, num_processes, t_sim, K_scaling, NEST_version)
 
 print("load simulation and network labels\n")
 
